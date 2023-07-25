6beef80dab0bced5586acea7590b54924f66921f
Mohamed Ben <b.mohamed@tomba.io>
2023-07-25 06:16:35 +0100

    chore: update

---
+++ pkg/validation/email/check.go
@@ -0,0 +1,38 @@
+package email
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
+	_tld "github.com/tomba-io/email/pkg/validation/tld"
+)
+
+// IsValidEmail Check if email is valid
+func IsValidEmail(email string) bool {
+
+	split := strings.Split(email, ".")
+	ending := split[len(split)-1]
+
+	if len(ending) < 2 {
+		return false
+	}
+
+	if len(email) > 254 {
+		return false
+	}
+	// check if TLD name actually exists and is not some image ending
+	if !_tld.IsValidTLD(ending) {
+		return false
+	}
+	// check with govalidator
+	if !govalidator.IsEmail(email) {
+		return false
+	}
+
+	if _, err := strconv.Atoi(ending); err == nil {
+		return false
+	}
+
+	return true
+}
