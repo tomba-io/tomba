a3f6a511e0e7dbddc72eca2de1fb4388e7baba2b
Mohamed Ben <b.mohamed@tomba.io>
2023-08-07 15:10:02 +0100

    feat: create Slack model

---
+++ pkg/slack/model.go
@@ -0,0 +1,33 @@
+package slack
+
+import "encoding/json"
+
+func UnmarshalSlack(data []byte) (Model, error) {
+	var r Model
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Model) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type Model struct {
+	Attachments []Attachment `json:"attachments"`
+}
+
+type Attachment struct {
+	Color  string  `json:"color"`
+	Blocks []Block `json:"blocks"`
+}
+
+type Block struct {
+	Type   string `json:"type"`
+	Text   Text   `json:"text"`
+	Fields []Text `json:"fields,omitempty"`
+}
+
+type Text struct {
+	Type string `json:"type"`
+	Text string `json:"text"`
+}
